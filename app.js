ed12f9883ab9a2424d743bbea53eb08207f400b4
christianmunozcaceres <christian.munozcaceres@gmail.com>
2021-03-30 18:24:33 +0200

    added simple get endpoint

--- app.js
+++ app.js
@@ -2,21 +2,29 @@ require("dotenv").config();
 
 const { Client } = require("pg");
 const client = new Client();
-client.connect();
+const database = client.connect();
 console.log("connected");
-/*
+
 const http = require("http");
 
 const hostname = "0.0.0.0";
 const port = process.env.PORT;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
+  if (req.url === "/exercises") {
+    const result = await client.query("select * from kickass.exercises;");
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.write(JSON.stringify(result.rows));
+    res.end();
+    return;
+  }
+
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+  res.end("hello there");
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-*/
--- query.sql
+++ query.sql
@@ -1,4 +1,4 @@
 CREATE SCHEMA kickass;
 CREATE TABLE IF NOT EXISTS kickass.exercises(id serial PRIMARY KEY,name varchar(50) UNIQUE NOT NULL);
 
-
+INSERT INTO kickass.exercises (name) VALUES ('Squats'), ('Deadlifts'), ('Bench Press');
